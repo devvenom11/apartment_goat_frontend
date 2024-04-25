8033da57784fda16ffd579028259230fd1d24fb7
jkr86 <johnyrj97@gmail.com>
2024-04-25 18:51:57 +0500

    make footer responsive on sm screen

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,43 +6,44 @@ const Footer = () => {
     return (
         <React.Fragment>
             <footer className='px-[45px] bg-[#FFFFFF] '>
-                <div className='flex  justify-between pt-[56px] pb-[92px]  sm:max-md:flex-col sm:max-md:pb-10 sm:max-md:pt-0 border border-yellow-800' >
+                <div className='flex  justify-between  md:flex-row md:pt-[56px] md:pb-[92px]  flex-col pb-10 pt-0' >
                     <div className=''>
                         <Link to={'/'}>
+                            {/* Logo width should be set in repsonivness */}
                             <Logo   />
                         </Link>
                     </div>
-                    <div className='max-w-[425px] w-full md:mx-auto mt-2.5 sm:max-md:mt-10'>
-                        <div className='flex mb-11 sm:max-md:mb-4  justify-between w-full sm:max-md:flex-col sm:max-md:gap-4'>
+                    <div className='max-w-[425px] w-full md:mx-auto md:mt-2.5 mt-10 mx-[0px]'>
+                        <div className='flex md:mb-11 md:flex-row  md:gap-0 mb-4   justify-between w-full flex-col gap-4'>
                             <Link to={'/'}>
-                                <p className='text-[#22223B] text-[16px] leading-[19px] sm:max-md:font-normal sm:max-md:font-Poppins'>About</p>
+                                <p className='text-[#22223B] text-[16px] leading-[19px] '>About</p>
                             </Link>
                             <Link to={'/'}>
-                                <p className='text-[#22223B] text-[16px] leading-[19px] sm:max-md:font-normal sm:max-md:font-Poppins'>Benefits</p>
+                                <p className='text-[#22223B] text-[16px] leading-[19px] '>Benefits</p>
                             </Link>
                             <Link to={'/'}>
-                                <p className='text-[#22223B] text-[16px] leading-[19px] sm:max-md:font-normal sm:max-md:font-Poppins'>Reviews</p>
+                                <p className='text-[#22223B] text-[16px] leading-[19px] '>Reviews</p>
                             </Link>
                         </div>
-                        <div className='flex justify-between w-full sm:max-md:flex-col sm:max-md:gap-4 '>
+                        <div className='flex justify-between  md:flex-row md:gap-0 w-full flex-col gap-4 '>
                             <Link to={'/'}>
-                                <p className='text-[#22223B] text-[16px] leading-[19px] sm:max-md:font-normal sm:max-md:font-Poppins'>FAQ</p>
+                                <p className='text-[#22223B] text-[16px] leading-[19px] '>FAQ</p>
                             </Link>
                             <Link to={'/'}>
-                                <p className='text-[#22223B] text-[16px] leading-[19px] sm:max-md:font-normal sm:max-md:font-Poppins'>How It Works</p>
+                                <p className='text-[#22223B] text-[16px] leading-[19px] '>How It Works</p>
                             </Link>
                             <Link to={'/'}>
-                                <p className='text-[#22223B] text-[16px] leading-[19px] sm:max-md:font-normal sm:max-md:font-Poppins'>Blog</p>
+                                <p className='text-[#22223B] text-[16px] leading-[19px] '>Blog</p>
                             </Link>
                         </div>
                     </div>
-                    <div className='mt-2.5 sm:max-md:mt-10'>
-                        <div className='mb-[25px] sm:max-md:mb-4'>
-                            <p className='font-semibold text-[19px] leading-[22px] sm:max-md:font-medium sm:max-md:font-Poppins sm:max-md:text-5 sm:max-md:leading-[26px]'>
+                    <div className='md:mt-2.5 mt-10'>
+                        <div className='mb-[25px] mb-4'>
+                            <p className='md:font-semibold md:text-[19px] md:leading-[22px] font-medium  text-5 leading-[26px]'>
                                 Our Community
                             </p>
                         </div>
-                        <div className='flex gap-6 sm:max-md:gap-[16px]'>
+                        <div className='flex md:gap-6 gap-[16px]'>
                             <Link to={'/'}>
                                 <div><CircleFacebookIcon /></div>
                             </Link>
@@ -55,8 +56,8 @@ const Footer = () => {
                         </div>
                     </div>
                 </div>
-                <div className='border-t pt-[34px] pb-[29px] sm:max-md:pt-[24px] sm:max-md:pb-6'>
-                    <p className='text-sm leading-[28px] text-[#22223B]'>Copyright © 2021 UI8 LLC. All rights reserved</p>
+                <div className='border-t md:pt-[34px] md:pb-[29px] pt-[24px] pb-6'>
+                    <p className='text-sm md:leading-[28px] text-[#22223B] font-normal leading-[21px]' >Copyright © 2021 UI8 LLC. All rights reserved</p>
                 </div>
             </footer>
         </React.Fragment>
