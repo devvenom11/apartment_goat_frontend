215aa82f323bc092d1ec86543290329e3ccac13a
jkr86 <johnyrj97@gmail.com>
2024-04-25 06:07:07 -0700

    faqs onclick changes

--- src/components/common/Accordian/index.js
+++ src/components/common/Accordian/index.js
@@ -10,11 +10,12 @@ const Accordian = ({question,answer}) => {
   return (
     <div className="">
      <div
-        className="bg-[#FEFEFE] rounded-[24px] p-[24px] mb-[10px] w-[635px]"
+        className="bg-[#FEFEFE] rounded-[24px] p-[24px] mb-[10px] w-[635px] cursor-pointer"
+        onClick={() => setOpenIndex (!openIndex)}
       >
         <div
           className="flex items-center justify-between cursor-pointer"
-          onClick={() => setOpenIndex (!openIndex)}
+        
         >
           <p className="font-[500] text-[16px] leading-[20.8px] text-[#22223B]">{question}</p>
           {openIndex ? <SheveronDown /> :  <SheveronUp /> }
